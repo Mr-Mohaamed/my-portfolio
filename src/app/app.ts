b0bd5ff50e5f2488b3101af606b18bea8e5175c4
MohamedMamdouh469 <Mohamed.Mamdouh@digital-solutionhouse.com>
2026-06-08 02:27:56 +0300

    Add gallery component and enhance project section with interactive buttons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,34 @@
+import { ProjectService } from './services/project-service';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectPage } from './project-page/project-page';
 import { ProjectSection } from './components/project-section/project-section';
 import { ProjectTypingComponent } from './components/project-typing-component/project-typing-component';
+import { GalleryComponent } from './components/gallery-component/gallery-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectPage, ProjectSection, ProjectTypingComponent],
+  imports: [RouterOutlet, ProjectPage, ProjectSection, ProjectTypingComponent, GalleryComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('portfolio');
+  constructor(public projectService: ProjectService) {}
+
+  selectProject(projectId: number) {
+    this.projectService.selectedProject.set(projectId);
+  }
+
+  isGalleryShown = signal(false);
+
+  openGallery() {
+    console.log('OPEN');
+    this.isGalleryShown.set(true);
+  }
+
+  closeGallery() {
+    console.log('CLOSE');
+    this.isGalleryShown.set(false);
+  }
 }
